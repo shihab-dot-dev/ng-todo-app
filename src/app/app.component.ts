ee42434bbfad8f81caa1e616d49a14a79c823fe4
shihabpm <shihab.pm.dev@gmail.com>
2025-04-07 22:09:16 +0530

    test some zed commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
 }
+
+function test() {
+  const obj = { foo: "shihab", bar: "pm" };
+  console.log(obj.bar);
+  const arr = [1, 2, 5, 8, 9, 10, 15, 26, 78];
+  return arr.filter((el) => {
+    return el > 15;
+  });
+}
+
+function testCol() {
+  const ar = [2, 8, 10, 19, 89, 45, 50, 57, 58];
+  ar.filter((el) => {
+    return el > 15;
+  });
+}
+console.log("returned value is", test());
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+console.log("hello");
+
+const arr = [5, 6, 7, 8, 0, 15];
+const test = arr.filter((e) => e > 5);
+console.log("test filter value is", test);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
